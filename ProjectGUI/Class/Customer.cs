77ce5eadd6f06f1d61727235172636f454d5d6d3
68.K.umnart <umnart@sunif.co.th>
2022-10-12 11:12:02 +0700

    change-1

---
+++ ProjectGUI/Class/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGUI
+{
+    class Customer
+    {
+        string name = "";
+        string Lastname = "";
+        string address = "";
+        string email = "";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Lastname1
+        {
+            get
+            {
+                return Lastname;
+            }
+
+            set
+            {
+                Lastname = value;
+            }
+        }
+    }
+}
---
+++ ProjectGUI/FrmCustomer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectGUI
+{
+    /// <summary>
+    /// Interaction logic for FrmCustomer.xaml
+    /// </summary>
+    public partial class FrmCustomer : Window
+    {
+        public FrmCustomer()
+        {
+            InitializeComponent();
+
+            Customer customer = new Customer();
+            customer.Name = "Jumbo";
+            customer.Lastname1 = "TwoYou";
+
+            MessageBox.Show("Hello" + "  " +customer.Name +"  "+ customer.Lastname1 );            
+        }
+    }
+}
